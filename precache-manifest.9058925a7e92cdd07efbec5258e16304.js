32c77d459839038604bc4f974bbd7e62f66fb46b
reshovroy <reshovr58@gmail.com>
2019-08-05 11:56:56 +0530

    Updates

---
+++ precache-manifest.9058925a7e92cdd07efbec5258e16304.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b5ed933057eaf1919dd79d81248cdbce",
+    "url": "/gawdsnet2/static/media/AvenirNextLTPro-Regular.b5ed9330.otf"
+  },
+  {
+    "revision": "91d8ff8ac25f51073a577408ae9cec86",
+    "url": "/gawdsnet2/static/media/AvenirNextLTPro-Bold.91d8ff8a.otf"
+  },
+  {
+    "revision": "58e7680875b82429dd5d",
+    "url": "/gawdsnet2/static/js/runtime~main.58e76808.js"
+  },
+  {
+    "revision": "215861d6bbc100a4a13d",
+    "url": "/gawdsnet2/static/js/main.215861d6.chunk.js"
+  },
+  {
+    "revision": "9920092d5f1a5dbab81d",
+    "url": "/gawdsnet2/static/js/2.9920092d.chunk.js"
+  },
+  {
+    "revision": "215861d6bbc100a4a13d",
+    "url": "/gawdsnet2/static/css/main.52ba84b3.chunk.css"
+  },
+  {
+    "revision": "552d2ddcb4aff699a3bbc6275a52a65c",
+    "url": "/gawdsnet2/index.html"
+  }
+];
